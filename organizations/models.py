6730bab60a95943d635c7047411a93926c0fc817
Maciej Stromich <maciej.stromich@gmail.com>
2017-09-15 12:37:35 +0200

    wip

--- organizations/models.py
+++ organizations/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.db.models import get_model
+from django.apps import apps
 from django.utils.importlib import import_module
 from django.utils.translation import ugettext_lazy as _
 
